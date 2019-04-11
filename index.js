efb165916258d961079ae995da0ce6dfbc83df9e
Dan Fletcher <dannyjfletcher@hotmail.com>
2019-04-10 20:19:41 -0400

    Add some shit

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 console.log('Hello, freeCodeCamp!')
 
 console.log('This is new')
+
+// git init
+// git status
+// git add
+// git commit -m "this will be all you need most the time"
+// git log
+
